b4f1131aba3344f0e02bcae3fba46accb7d9398f
Matt Dobaj <85978036+dobaj@users.noreply.github.com>
2025-05-23 15:35:23 -0400

    make background movement more stable

--- src/App.tsx
+++ src/App.tsx
@@ -4,48 +4,48 @@ import { Background } from "./components/Background";
 
 function App() {
   const name = "Hi! I'm Matt";
-  
 
   return (
     <>
-      {/* <Background> */}
       <Background>
-      <h1>
-        {Array.from(name).map((letter, index) => {
-          let rotation = 9;
+        <h1 aria-label={name}>
+          <span aria-hidden="true">
+            {Array.from(name).map((letter, index) => {
+              let rotation = 6;
 
-          if (index % 2 == 0) {
-            rotation *= -1;
-          }
+              if (index % 2 == 0) {
+                rotation *= -1;
+              }
 
-          return (
-            <span
-              key={index}
-              style={{
-                display: "inline-block",
-                transform: `rotate(${rotation}deg)`,
-                WebkitTransform: `rotate(${rotation}deg)`,
-                marginRight: "2px",
-              }}
-            >
-              {letter}
-            </span>
-          );
-        })}
-      </h1>
-      <h3>Welcome to my page!</h3>
-      <img src={me}></img>
-      <h3>content</h3>
-      <h3>content</h3>
-      <h3>content</h3>
-      <h3>content</h3>
-      <h3>content</h3>
-      <h3>content</h3>
-      <h3>content</h3>
-      <h3>content</h3>
-      <h3>content</h3>
-      <h3>content</h3>
-    </Background>
+              return (
+                <span
+                  key={index}
+                  style={{
+                    display: "inline-block",
+                    transform: `rotate(${rotation}deg)`,
+                    WebkitTransform: `rotate(${rotation}deg)`,
+                    marginRight: "2px",
+                  }}
+                >
+                  {letter}
+                </span>
+              );
+            })}
+          </span>
+        </h1>
+        <h3>Welcome to my page!</h3>
+        <img src={me}></img>
+        <h3>content</h3>
+        <h3>content</h3>
+        <h3>content</h3>
+        <h3>content</h3>
+        <h3>content</h3>
+        <h3>content</h3>
+        <h3>content</h3>
+        <h3>content</h3>
+        <h3>content</h3>
+        <h3>content</h3>
+      </Background>
     </>
   );
 }
--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,25 +1,19 @@
-import {
-  useEffect,
-  useRef,
-  type ReactNode,
-  type RefObject,
-} from "react";
+import { useEffect, useRef, type ReactNode, type RefObject } from "react";
 import "./Background.css";
 
 export const Background = ({ children }: { children: ReactNode }) => {
   const noiseRef = useRef<HTMLDivElement | null>(null);
   const backgroundRef = useRef<HTMLDivElement | null>(null);
 
-  // const initialMouseRef = useRef<{
-  //   x: number;
-  //   y: number;
-  // }>({ x: -1, y: -1 });
-
-  
   const mouseRef = useRef<{
     x: number;
     y: number;
-  }>({ x: 0, y: 0 });
+  }>({ x: -1, y: -1 });
+
+  const scrollRef = useRef<{
+    x: number;
+    y: number;
+  }>({ x: -1, y: -1 });
 
   const noisePosRef = useRef<{
     x: number;
@@ -31,7 +25,8 @@ export const Background = ({ children }: { children: ReactNode }) => {
     y: number;
   }>({ x: 0, y: 0 });
 
-  const clampDelta = (newVal: number, oldVal: number, maxDelta = 0.5) => {
+  const clampDelta = (newVal: number, oldVal: number, maxDelta = 1) => {
+    // Make it so that a value can only change by one pixel max per fram
     const delta = newVal - oldVal;
     if (Math.abs(delta) > maxDelta) {
       return oldVal + Math.sign(delta) * maxDelta;
@@ -39,19 +34,25 @@ export const Background = ({ children }: { children: ReactNode }) => {
     return newVal;
   };
 
-  const setBgOffset = (
+  const moveRef = (
     objectRef: RefObject<HTMLDivElement | null>,
     ref: RefObject<{ x: number; y: number }>,
     x: number,
     y: number
   ) => {
     if (objectRef && objectRef.current) {
-      const beforeStyles = getComputedStyle(objectRef.current, "::before");
-      const zIndex = beforeStyles.getPropertyValue("z-index");
+      const beforeStyle = getComputedStyle(objectRef.current, "::before");
+      const zIndex = beforeStyle.getPropertyValue("z-index");
+      const multiplier = 100 + parseFloat(zIndex); // arbitrary 100 baseline
 
-      const multiplier = 100 + parseFloat(zIndex); // arbitrary 100
-      x = x * multiplier;
-      y = y * multiplier;
+      let bgX = parseFloat(objectRef.current.style.getPropertyValue("--bg-x"));
+      let bgY = parseFloat(objectRef.current.style.getPropertyValue("--bg-y"));
+
+      if (isNaN(bgX)) bgX = 0;
+      if (isNaN(bgY)) bgY = 0;
+
+      x = bgX + x * multiplier;
+      y = bgY + y * multiplier;
 
       x = clampDelta(x, ref.current.x);
       y = clampDelta(y, ref.current.y);
@@ -64,44 +65,53 @@ export const Background = ({ children }: { children: ReactNode }) => {
     }
   };
 
-  const calcMove = (mousePos: number, scrollPos: number) => {
-    return (mousePos - scrollPos * 2) / window.innerWidth - 0.5;
+  const moveBgs = (moveX: number, moveY: number) => {
+    moveRef(noiseRef, noisePosRef, moveX, moveY);
+    moveRef(backgroundRef, bgPosRef, moveX, moveY);
   };
-  
+
+  const calcMove = (mousePos: number, scrollPos: number) => {
+    return mousePos / 2000 - scrollPos / 1000;
+  };
+
   useEffect(() => {
-    const handleMouseMove = (e: {
-      clientY: number;
-      clientX: number;
-      pageX: number;
-      pageY: number;
-    }) => {
-      // if (initialMouseRef.current.x == -1 && initialMouseRef.current.y == -1) {
-      //   console.log("here")
-      //   initialMouseRef.current.x = e.clientX
-      //   initialMouseRef.current.y = e.clientY
-      // }
-      const moveX = calcMove(e.clientX, window.scrollX);
-      const moveY = calcMove(e.clientY, window.scrollY);
+    const handleMouseMove = (e: { clientY: number; clientX: number }) => {
+      if (mouseRef.current.x == -1 && mouseRef.current.y == -1) {
+        mouseRef.current.x = e.clientX;
+        mouseRef.current.y = e.clientY;
+        return;
+      }
+
+      // Calculate how much to move backgrounds (before multiplying)
+      const moveX = calcMove(e.clientX - mouseRef.current.x, 0);
+      const moveY = calcMove(e.clientY - mouseRef.current.y, 0);
 
       mouseRef.current.x = e.clientX;
       mouseRef.current.y = e.clientY;
 
-      setBgOffset(noiseRef, noisePosRef, moveX, moveY);
-      setBgOffset(backgroundRef, bgPosRef, moveX, moveY);
+      moveBgs(moveX, moveY);
     };
 
     const handleScroll = () => {
-      const moveX = calcMove(mouseRef.current.x, window.scrollX);
-      const moveY = calcMove(mouseRef.current.y, window.scrollY);
+      if (scrollRef.current.x == -1 && scrollRef.current.y == -1) {
+        scrollRef.current.x = window.scrollX;
+        scrollRef.current.y = window.scrollY;
+        return;
+      }
 
-      setBgOffset(noiseRef, noisePosRef, moveX, moveY);
-      setBgOffset(backgroundRef, bgPosRef, moveX, moveY);
+      // Calculate how much to move backgrounds (before multiplying)
+      const moveX = calcMove(0, window.scrollX - scrollRef.current.x);
+      const moveY = calcMove(0, window.scrollY - scrollRef.current.y);
+
+      scrollRef.current.x = window.scrollX;
+      scrollRef.current.y = window.scrollY;
+
+      moveBgs(moveX, moveY);
     };
 
     window.addEventListener("mousemove", handleMouseMove);
     window.addEventListener("scroll", handleScroll);
 
-    // Cleanup listener on unmount
     return () => {
       window.removeEventListener("mousemove", handleMouseMove);
       window.removeEventListener("scroll", handleScroll);
